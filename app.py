9b6010c824a617112ae4d1a98cf0b6a1650e01e5
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-04 19:37:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import requests
+from truera.client.truera_workspace import TrueraWorkspace
+from truera.client.truera_authentication import TokenAuthentication
 
-def fetch_data(api_key):
+# Replace these placeholders with your actual TruEra URL and token
+TRUERA_URL = "<YOUR_TRUERA_URL>"
+TOKEN = "<YOUR_TRUERA_TOKEN>"
+
+auth = TokenAuthentication(TOKEN)
+tru = TrueraWorkspace(TRUERA_URL, auth)
+
+def fetch_data(api_key, endpoint):
     # Replace 'YOUR_TRUERA_API_ENDPOINT' with the actual TruEra API endpoint
-    api_endpoint = 'YOUR_TRUERA_API_ENDPOINT'
+    api_endpoint = f'{TRUERA_URL}/{endpoint}'
     
     # Replace 'YOUR_TRUERA_API_KEY' with the provided API key
     headers = {'Authorization': f'Bearer {api_key}'}
@@ -26,12 +35,16 @@ def main():
     
     if api_key:
         # Fetch data using the provided API key
-        data = fetch_data(api_key)
-        
-        if data:
-            # Display the fetched data
-            st.write("Fetched Data:")
-            st.write(data)
-    
+        st.subheader("Example Endpoints:")
+        for i in range(1, 11):
+            endpoint = f"example_endpoint_{i}"
+            st.write(f"Endpoint {i}: {endpoint}")
+            data = fetch_data(api_key, endpoint)
+            
+            if data:
+                # Display the fetched data for each endpoint
+                st.write(f"Fetched Data for Endpoint {i}:")
+                st.write(data)
+
 if __name__ == "__main__":
     main()
